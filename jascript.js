e9bf2c0bb2993098870183af31c015888b3a40e6
thiwankadilshanliyanage <thiwankadilshanhub98@gmail.com>
2021-10-19 14:05:43 +0530

    first commit

--- jascript.js
+++ jascript.js
@@ -33,15 +33,41 @@ console.dir(document);
 
 
 //select elements using ID
-console.log(document.getElementById('frmt'));
-var formTitle = document.getElementById('frmt');
-console.log(formTitle);
 
-// --textContent, innerText & innerHTML --
-formTitle.textContent = 'Add Shopping Items';
+// console.log(document.getElementById('frmt'));
+// var formTitle = document.getElementById('frmt');
+// console.log(formTitle);
+
+// --textContent, innerT/ext & innerHTML --
+
+// formTitle.textContent = 'Add Shopping Items';
 //formTitle.innerText = 'Shopping Items';
 //formTitle.innerHTML = '<i>Hello </i> Add Items';
 
-console.log(formTitle.textContent);
+// console.log(formTitle.textContent);
 //console.log(formTitle.innerText);
 //console.log(formTitle.innerHTML);
+
+// styling
+
+// var navigation = document.getElementById('navbar');
+// navigation.style.backgroundColor = '#090209';
+// navigation.style.color = '#fff';
+
+// select element using class name
+
+var items = document.getElementsByClassName('items');
+console.log(items);
+console.log(items[0]);
+console.log(items[2]);
+items[2].textContent = "Hand Bag";
+console.log(items[2]);
+
+//Styling
+items[2].style.fontFamily = 'serif';
+items[2].style.backgroundColor = '#ebca14';
+
+//change back color of all the list items
+for (var i of items) {
+    i.style.backgroundColor = '#a7520c';
+}
